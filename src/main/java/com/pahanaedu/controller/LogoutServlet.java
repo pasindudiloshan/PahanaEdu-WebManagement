59720e5781b456c606de5734e0c7bc65ad0b395a
PasinduDiloshan27 <pasindudiloshan@gmail.com>
2025-08-16 19:42:13 +0530

    Fix: login attempts with SweetAlert feedback

---
+++ src/main/java/com/pahanaedu/controller/LogoutServlet.java
@@ -0,0 +1,19 @@
+package com.pahanaedu.controller;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/Logout")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        session.invalidate();
+        response.sendRedirect("login.jsp");
+    }
+}
